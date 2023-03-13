6a9051e595bfb4cc45fe4dfc00c6aaf04d9d9022
Manuel G. Araujo A <geronimoarteaga@gmail.com>
2023-03-13 12:17:40 -0300

    Initial commit ...

---
+++ src/main/java/com/ey/cl/springboot/rest/usercreation/UserCreationSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.ey.cl.springboot.rest.usercreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreationSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCreationSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/cl/springboot/rest/usercreation/UserCreationSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.cl.springboot.rest.usercreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreationSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
